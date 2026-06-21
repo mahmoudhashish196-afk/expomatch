6b0e6f1dec5e7964ea15a68fff9fd058cb295162
mahmoudhashish196-afk <mahmoudhashish196@gmail.com>
2026-06-21 13:22:47 +0200

    fix supabase url and keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://usbtlexntpiovhkfqnlb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzYnRsZXhudHBpb3Zoa2ZxbmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MTMwODgsImV4cCI6MjA5NzI4OTA4OH0.WNCMHwlI5RcmVB3107Zk91E6akSXidfHn3fCFVpY1Ms';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
